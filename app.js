4d8119e30b65048fbb4226bd8fd4cd687db8088b
Niko_Butenko <174657090+Butenkite@users.noreply.github.com>
2025-01-30 14:59:12 -0800

    Data now sends

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const app = express();
 // serve static files from public
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: true }));
+
 // Define PORT
 const PORT = 3000;
+
+// store data
 const d = new Date();
 const appointments = [];
 // home page
@@ -16,12 +20,13 @@ app.get('/', (req, res) =>{
     res.sendFile(`${import.meta.dirname}/views/home.html`);
 });
 
+// confirmation
 app.post('/confirmation', (req, res) =>{
     let minutes = d.getMinutes();
     appointments.push(req.body);
     appointments.push(`Time: ${minutes}`);
     res.send(req.body);
-    console.log(req.body)
+    console.log(req.body);
 });
 
 app.get('/admin/appointments', (req, res) =>{
