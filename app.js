c6e372a874c0987e4015d5166d3ecfd9740ea5d1
Jacob Woodbury <166659344+JacobWoodbury@users.noreply.github.com>
2025-01-30 14:38:51 -0800

    post

--- app.js
+++ app.js
@@ -8,12 +8,22 @@ app.use(express.static('public'));
 
 // Define PORT
 const PORT = 3000;
-
+const d = new Date();
+const appointments = [];
 // home page
 app.get('/', (req, res) =>{
 
     res.sendFile(`${import.meta.dirname}/views/home.html`);
 });
+
+app.post('/submitButton', (req, res) =>{
+    let minutes = d.getMinutes();
+    appointments.push(req.body);
+    appointments.push(`Time: ${minutes}`);
+    res.send(req.body);
+    
+});
+
 
 // listen to PORT
 app.listen(PORT, () =>{
